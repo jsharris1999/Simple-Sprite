084b238020f6677f8f8faf07c6befa03e2f28af3
Jared Harris <Jared Harris>
2017-02-19 22:41:13 -0600

    first commit

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Drawing.Drawing2D;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static WindowsFormsApplication1.Sprite;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public static Sprite parent;
+        public static Form1 form;
+        public Thread ActThread;
+        public Thread PaintThread;
+        public static int fps = 60;
+        public static double running_fps = 60.0;
+
+        public Form1()
+        {
+            DoubleBuffered = true;
+            InitializeComponent();
+            parent = new Sprite();
+            form = this;
+            parent.add(new Picture(803, 1536));
+            PaintThread = new Thread(new ThreadStart(render));
+            ActThread = new Thread(new ThreadStart(update));
+            PaintThread.Start();
+            ActThread.Start();
+        }
+
+        public static void render()
+        {
+            DateTime last = DateTime.Now;
+            DateTime now = last;
+            TimeSpan frameTime = new TimeSpan(10000000 / fps);
+            while (true)
+            {
+                DateTime temp = DateTime.Now;
+                running_fps = .9 * running_fps + .1 * 1000.0 / (temp - now).TotalMilliseconds;
+                now = temp;
+                TimeSpan diff = now - last;
+                if (diff.TotalMilliseconds < frameTime.TotalMilliseconds)
+                    Thread.Sleep((frameTime - diff).Milliseconds);
+                last = DateTime.Now;
+                form.Invoke(new MethodInvoker(form.Refresh));
+
+            }
+        }
+
+        public static void update()
+        {
+            DateTime last = DateTime.Now;
+            DateTime now = last;
+            TimeSpan frameTime = new TimeSpan(10000000 / fps);
+            while (true)
+            {
+                DateTime temp = DateTime.Now;
+                now = temp;
+                TimeSpan diff = now - last;
+                if (diff.TotalMilliseconds < frameTime.TotalMilliseconds)
+                    Thread.Sleep((frameTime - diff).Milliseconds);
+                last = DateTime.Now;
+                parent.update();
+
+            }
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            //Refresh();
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+            Console.WriteLine(ClientSize.Height);
+            Console.WriteLine(ClientSize.Width);
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            ActThread.Abort();
+            PaintThread.Abort();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            parent.render(e.Graphics);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Sprite.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Drawing.Drawing2D;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public class Sprite
+    {
+        private Sprite parent = null;
+
+        //instance variable
+        private float x = 0;
+
+        public float X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        private float y = 0;
+
+        public float Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+
+        private float scale = 1;
+
+        public float Scale
+        {
+            get { return scale; }
+            set { scale = value; }
+        }
+
+        private float rotation = 0;
+
+        /// <summary>
+        /// The rotation in degrees.
+        /// </summary>
+        public float Rotation
+        {
+            get { return rotation; }
+            set { rotation = value; }
+        }
+
+
+        public List<Sprite> children = new List<Sprite>();
+
+
+        public void Kill()
+        {
+            parent.children.Remove(this);
+        }
+
+        //methods
+        public void render(Graphics g)
+        {
+            Matrix original = g.Transform.Clone();
+            g.TranslateTransform(x, y);
+            g.ScaleTransform(scale, scale);
+            g.RotateTransform(rotation);
+            paint(g);
+            foreach (Sprite s in children)
+            {
+                s.render(g);
+            }
+            g.Transform = original;
+        }
+
+        public void update()
+        {
+            act();
+            foreach (Sprite s in children)
+            {
+                s.update();
+            }
+        }
+
+        public virtual void paint(Graphics g)
+        {
+
+        }
+
+        public virtual void act()
+        {
+
+        }
+
+        public void add(Sprite s)
+        {
+            s.parent = this;
+            children.Add(s);
+        }
+
+        public class Picture : Sprite
+        {
+            Image hydralisk = Image.FromFile("hydralisk.png", true);
+            public int xc;
+            public int yc;
+            public int s = 0;
+            public int width;
+            public int height;
+
+            public Picture(int h, int w) : base()
+            {
+                Random rand = new Random();
+                xc = rand.Next(h);
+                yc = rand.Next(h);
+                width = w;
+                height = h;
+            }
+
+            public override void act()
+            {
+                xc = (int) (Math.Sin(s/4) * 400 + 200) % (width - 100);
+                yc = (int) (Math.Cos(s/4) * 400 + 200) % (height - 100);
+                s++;
+            }
+
+            public override void paint(Graphics g)
+            {
+                g.DrawString("Hey", new Font("Comic Sans MS", 10), Brushes.Crimson, xc, yc);
+                g.DrawImage(hydralisk, xc, yc);
+                
+            }
+
+        }
+    }
+}
